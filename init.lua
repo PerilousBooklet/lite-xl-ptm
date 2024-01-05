d40863b4c90690efdf5af28c68663fc42d150892
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-01-05 19:31:03 +0100

    Added some things.

--- init.lua
+++ init.lua
@@ -14,26 +14,26 @@ config.plugins.ptm = {}
 -- Template generation
 local function template_generation(title)
 	local dir = title
-	local wd = os.execute("pwd")
-	-- TODO: Windows/Mac/Linux support (check which operating system is running)
+	-- Get current working directory
+	local wd = os.getenv("PWD")
 	-- TODO: check if there is a directory with the same name as the one from user input; if there is, refuse further instructions
 	
 	-- Create project directory
-	system.mkdir(wd .. dir)
+	print(wd) --debug
+	system.mkdir(wd .. "/" .. dir)
   
-	-- Create files
   -- TODO: get files list (each file is assigned a string of text to contain)
   -- TODO: write iterator to create and fill files
-  local filename
   -- TODO: get and assign filename from template file
 	-- TODO: get and assign multi-line strings from template file
 	-- TODO: write to the files the contents of the multi-line string tables
-  os.execute("touch " .. wd .. "/" .. dir .. "/" .. filename)
+	-- Create files
+  local filename = "abcd.sh"
 	local file = io.open(wd .. "/" .. dir .. "/" .. filename, "w")
   
-  local string = ""
-  file:write(string)
-  
+  -- Write file contents
+  local mls = "Multi-line string."
+  file:write(mls)
   file:close()
 end
 
@@ -41,50 +41,55 @@ end
 -- Template selection
 local template_selection = function(t, title)
 	-- Switch-case function implementation
-    local switch = function(t)
-        local case = {
-        -- C++ simple
-        ["cpp-simple"] = function()
-          -- Create template from template description file located in ./templates
-          template_generation(title)
-        end,
-        -- Java simple
-        ["java-simple"] = function()
-          print("java")
-        end,
-        -- Arch Linux's PKGBUILD
-        ["pkgbuild-simple"] = function()
-          print(t)
-        end,
-        -- Default case
-        default = function()
-          -- TODO: spawn warning in status bar
-          print("WARNING: the input didn't match any of the predefined templates!")
-        end,
-        }
-        -- Selection logic
-        if case[t] then
-          case[t]()
-        else
-          case["default"]()
-        end
+  local switch = function(t)
+    local case = {
+    -- C++ simple
+    ["cpp-simple"] = function()
+      -- TODO: get files table entry number
+      -- TODO: get files table
+      -- Create template from template description file located in ./templates
+      template_generation(title)
+    end,
+    -- Java simple
+    ["java-simple"] = function()
+      print("java")
+    end,
+    -- Arch Linux's PKGBUILD
+    ["pkgbuild-simple"] = function()
+      print(t)
+    end,
+    -- Default case
+    default = function()
+      -- TODO: spawn warning in status bar
+      print("WARNING: the input didn't match any of the predefined templates!")
+    end,
+    }
+    -- Selection logic
+    if case[t] then
+      case[t]()
+    else
+      case["default"]()
     end
-    switch(t)
+  end
+  switch(t)
 end
 
 -- ?
 local project_template_manager = function()
   -- TODO: Implement template name suggestions list above command view
-  -- Get the input text for the template name
+  -- Get input text for template name
   core.command_view:enter("Choose template", {
     submit = function(text)
-      -- Get the input text for the project title
+      -- Get input text for project folder title
       core.command_view:enter("Choose project title", {
         submit = function(title)
           -- Submit chosen template for selection
           template_selection(text, title)
         end
       })
+    end,
+    suggest = function()
+    	-- ?
     end
   })
 end
@@ -97,4 +102,4 @@ command.add(nil, {
 })
 
 -- Key bindings
---keymap.add { ["ctrl+shift+alt+p"] = "ptm:choose-template" }
+keymap.add { ["ctrl+shift+alt+p"] = "ptm:choose-template" }
